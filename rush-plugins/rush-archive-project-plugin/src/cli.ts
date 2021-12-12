f35826b71829f6000e0b930610cc2bf3c204da9e
Cheng Liu <chengcyber@outlook.com>
2021-12-12 22:26:53 +0800

    :sparkles: unarchive impl

--- rush-plugins/rush-archive-project-plugin/src/cli.ts
+++ rush-plugins/rush-archive-project-plugin/src/cli.ts
@@ -2,7 +2,6 @@
 
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
-import { archive } from "./commands/archive";
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 main();
@@ -21,8 +20,26 @@ async function main(): Promise<void> {
           .demandOption(["packageName"]);
       },
       async (argv) => {
+        const { archive } = await import("./commands/archive");
         await archive(argv);
         console.log("Archive ALL DONE!");
+      }
+    )
+    .command(
+      "unarchive",
+      "Unarchive a project",
+      (yargs) => {
+        return yargs
+          .option("packageName", {
+            type: "string",
+            describe: "The name of the package to unarchive",
+          })
+          .demandOption(["packageName"]);
+      },
+      async (argv) => {
+        const { unarchive } = await import("./commands/unarchive");
+        await unarchive(argv);
+        console.log("Unarchive ALL DONE!");
       }
     )
     .demandCommand(1, "You need at least one command before moving on")
---
+++ rush-plugins/rush-archive-project-plugin/src/commands/unarchive.ts
@@ -0,0 +1,97 @@
+import * as path from "path";
+import { RushConfiguration } from "@microsoft/rush-lib";
+import { FileSystem, JsonFile, JsonObject } from "@rushstack/node-core-library";
+import ora from "ora";
+import { getGraveyardInfo } from "../logic/graveyard";
+import * as tar from "tar";
+import { ProjectMetadata } from "../logic/projectMetadata";
+
+export interface IUnarchiveConfig {
+  packageName: string;
+}
+
+export async function unarchive({
+  packageName,
+}: IUnarchiveConfig): Promise<void> {
+  let rushConfiguration: RushConfiguration | undefined;
+  let spinner: ora.Ora | undefined;
+  try {
+    rushConfiguration = RushConfiguration.loadFromDefaultLocation({
+      startingFolder: process.cwd(),
+    });
+  } catch {
+    // no-catch
+  }
+  if (!rushConfiguration) {
+    throw new Error("Could not load rush configuration");
+  }
+
+  // check if package name is existing
+  if (rushConfiguration.getProjectByName(packageName)) {
+    throw new Error(`Package ${packageName} already exists`);
+  }
+
+  const monoRoot: string = rushConfiguration.rushJsonFolder;
+
+  // extract tarball
+  const { tarballName, tarballFolder } = getGraveyardInfo({
+    monoRoot,
+    packageName,
+  });
+  const tarballPath: string = path.join(tarballFolder, tarballName);
+  if (!FileSystem.exists(tarballPath)) {
+    throw new Error(
+      `Could not find tarball ${tarballName} for package name ${packageName}`
+    );
+  }
+  spinner = ora(`Extracting ${tarballName}`).start();
+  // tar xf <package_name>.tar.gz -C
+  const extractWorkingFolder: string = path.join(
+    tarballFolder,
+    packageName.replace("/", "!")
+  );
+  FileSystem.ensureFolder(extractWorkingFolder);
+  tar.extract({
+    file: path.join(tarballFolder, tarballName),
+    sync: true,
+    cwd: extractWorkingFolder,
+  });
+  spinner.succeed();
+
+  // read metadata
+  const projectMetadata: ProjectMetadata = ProjectMetadata.load(
+    path.join(extractWorkingFolder, ProjectMetadata.FILENAME)
+  );
+
+  const projectConfig: JsonObject = projectMetadata.projectConfig;
+  const projectRelativeFolder: string = projectConfig.projectFolder;
+  const projectFolder: string = path.join(monoRoot, projectRelativeFolder);
+  if (FileSystem.exists(projectFolder)) {
+    throw new Error(`Project folder ${projectRelativeFolder} already exists`);
+  }
+
+  // move to project folder
+  spinner = ora(`Moving code to ${projectRelativeFolder}`).start();
+  FileSystem.move({
+    sourcePath: extractWorkingFolder,
+    destinationPath: projectFolder,
+  });
+  spinner.succeed();
+
+  // add to rush config
+  spinner = ora(`Resuming project configuration to rush.json`).start();
+  const rawRushJson: JsonObject = JsonFile.load(rushConfiguration.rushJsonFile);
+  rawRushJson.projects.push(projectConfig);
+  JsonFile.save(rawRushJson, rushConfiguration.rushJsonFile, {
+    updateExistingFile: true,
+    prettyFormatting: true,
+  });
+  spinner.succeed();
+
+  spinner = ora(`Housekeeping`).start();
+  // delete metadata
+  FileSystem.deleteFile(path.join(projectFolder, ProjectMetadata.FILENAME));
+  // remove tarball from graveyard
+  FileSystem.deleteFile(tarballPath);
+  spinner.succeed();
+}
