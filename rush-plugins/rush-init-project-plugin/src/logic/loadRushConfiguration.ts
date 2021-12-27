9769cb8bcef776fa947ee803f60b2225396caa6e
Cheng Liu <chengcyber@outlook.com>
2021-12-27 16:28:11 +0800

    fix(rush-init-project-plugin): workaround for rush version validality

--- rush-plugins/rush-init-project-plugin/src/logic/loadRushConfiguration.ts
+++ rush-plugins/rush-init-project-plugin/src/logic/loadRushConfiguration.ts
@@ -1,4 +1,5 @@
 import { RushConfiguration } from "@microsoft/rush-lib";
+import { JsonFile, JsonObject } from "@rushstack/node-core-library";
 
 const cwd2RushConfiguration: Record<string, RushConfiguration> = {};
 
@@ -8,9 +9,19 @@ export const loadRushConfiguration = (
   let rushConfiguration: RushConfiguration = cwd2RushConfiguration[cwd];
   if (!rushConfiguration) {
     try {
-      rushConfiguration = RushConfiguration.loadFromDefaultLocation({
-        startingFolder: cwd,
-      });
+      const rushJsonFilePath: string | undefined =
+        RushConfiguration.tryFindRushJsonLocation({
+          startingFolder: cwd,
+        });
+      if (!rushJsonFilePath) {
+        throw new Error(`Not found rush.json`);
+      }
+      const rushJson: JsonObject = JsonFile.load(rushJsonFilePath);
+      // HACK!!! await merge this PR https://github.com/microsoft/rushstack/pull/3091
+      rushConfiguration = new (RushConfiguration as any)(
+        rushJson,
+        rushJsonFilePath
+      );
       if (!rushConfiguration) {
         throw new Error("Rush configuration not found");
       }
