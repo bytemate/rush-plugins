7a7880f21047d2730bbfa8096b0c23106024c312
nana <29034500+nanaSun@users.noreply.github.com>
2022-12-07 15:21:47 +0800

    fix: git config user name

--- rush-plugins/rush-init-project-plugin/src/logic/GitConfig.ts
+++ rush-plugins/rush-init-project-plugin/src/logic/GitConfig.ts
@@ -1,23 +1,20 @@
-import * as child_process from 'child_process';
+import { Executable } from "@rushstack/node-core-library";
+import type { SpawnSyncReturns } from "child_process";
 
 export const getGitUserName = (cwd: string): string => {
-
-  const options: child_process.SpawnSyncOptions = {
-    cwd,
-    shell: true,
-    stdio: ['pipe', 'pipe', 'pipe'],
-    env: {}
-  };
   try {
-    const result: child_process.SpawnSyncReturns<Buffer> = child_process.spawnSync(
+    const result: SpawnSyncReturns<string> =  Executable.spawnSync(
       'git',
       ['config', 'user.name'],
-      options
+      {
+        currentWorkingDirectory: cwd,
+        stdio: "pipe",
+      }
     );
     if (result.status !== 0) {
       return '';
     }
-    return result.stdout.toString() ?? '';
+    return result.stdout?.trim() ?? '';
   } catch (e) {
     // no-catch
     return '';
