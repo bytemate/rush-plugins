f1e905dffad55022cc8f58b7f525315ef40e1159
William Huang <william2958@users.noreply.github.com>
2023-01-17 11:27:16 -0800

    feat: add ability to set configs for archive plugin

--- rush-plugins/rush-archive-project-plugin/src/logic/graveyard.ts
+++ rush-plugins/rush-archive-project-plugin/src/logic/graveyard.ts
@@ -1,5 +1,7 @@
 import * as path from "path";
-import { PackageName, IParsedPackageName } from "@rushstack/node-core-library";
+import { PackageName, IParsedPackageName, JsonFile } from "@rushstack/node-core-library";
+import { RushConfiguration } from "@rushstack/rush-sdk";
+import { loadRushConfiguration } from "./rushConfiguration";
 
 export const graveyardRelativeFolder: string = "common/_graveyard";
 
@@ -14,16 +16,44 @@ export interface IGetGraveyardInfoParams {
   packageName: string;
 }
 
+export interface IArchiveConfig {
+  graveyardFolder?: string
+}
+
 export const getGraveyardInfo = ({
   monoRoot,
-  packageName,
+  packageName
 }: IGetGraveyardInfoParams): IGraveyardInfo => {
+  const rushConfiguration: RushConfiguration = loadRushConfiguration();
+  // Check for config file
+
+  const pluginOptionsJsonFilePath: string = path.join(
+    rushConfiguration.rushPluginOptionsFolder,
+    `rush-archive-project-plugin.json`
+  );
+
+  try {
+  const archiveConfigs: IArchiveConfig | undefined = JsonFile.load(pluginOptionsJsonFilePath);
+
+    console.log(
+      `Found target graveyard in ${pluginOptionsJsonFilePath}`
+    );
+
+    console.log('configs: ', archiveConfigs);
+    process.exit(1);
+  } catch (e) {}
+
   const parsedPackageName: IParsedPackageName = PackageName.parse(packageName);
   const tarballRelativeFolder: string = path.join(
     graveyardRelativeFolder,
     parsedPackageName.scope
   );
   const tarballFolder: string = path.join(monoRoot, tarballRelativeFolder);
+  console.log({
+    tarballFolder,
+    tarballRelativeFolder,
+    tarballName: `${parsedPackageName.unscopedName}.tar.gz`,
+  });
   return {
     tarballFolder,
     tarballRelativeFolder,
